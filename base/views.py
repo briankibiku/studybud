6d4776f6ce68b1f07a396ee1c7cb1dbfa0677871
kibiku <kibikukim@gmail.com>
2022-08-12 07:05:59 +0300

    register user

--- base/views.py
+++ base/views.py
@@ -1,13 +1,16 @@
+from atexit import register
+from multiprocessing import context
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import Room, Topic
-from .forms import RoomForm
 from django.db.models import  Q
 from django.contrib.auth.models import User
 from django.contrib import  messages
 from django.contrib.auth import authenticate, login, logout
+from .models import Room, Topic
+from .forms import RoomForm
+from django.contrib.auth.forms import UserCreationForm
 
 
 rooms = [
@@ -21,9 +24,13 @@ rooms = [
 ]
 # Create your views here.
 def loginPage(request):
+    page = 'login'
+
+    if request.user.is_authenticated:
+        return redirect('home')
 
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
         try:
             user = User.objects.get(username=username)
@@ -38,8 +45,25 @@ def loginPage(request):
         else:
             messages.error(request, 'Username OR password does not exist')
 
-    context = {}
+    context = {'page': page}
     return render(request, 'base/login_register.html', context)
+
+def registerPage(request):
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "An error occured during registration")
+
+    return render(request, 'base/login_register.html', {'form': form})
+
 
 def logoutUser(request):
     logout(request)
@@ -106,5 +130,3 @@ def deleteRoom(request, pk):
         room.delete()
         return redirect('home') 
     return render(request, 'base/delete.html', {'obj': room})
-
-
